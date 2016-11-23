703ce512d01a82822a76ccadab0f1d4fac7cbb51
Gerard Maas <gerard.maas@gmail.com>
2016-11-23 21:37:48 +0100

    add raffle endpoints

--- app/controllers/RaffleController.scala
+++ app/controllers/RaffleController.scala
@@ -1,10 +1,25 @@
 package controllers
 
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
+
 
 class RaffleController extends Controller {
 
   def test = Action {
-    Ok("Hello")
+    Ok("{Hello")
   }
+
+  def createRaffle(name : String) = Action { request =>
+    Ok(Json.toJson(Map("result"->"ok")))
+  }
+
+  def addParticipant(id:String, username:String) = Action { request =>
+    Ok(Json.toJson(Map("result"->s"added participant $username to raffle $id")))
+  }
+
+  def listParticipants(id:String) = Action {request =>
+    Ok(Json.toJson(Map("result"->s"listing users for raffle $id")))
+  }
+
 }
