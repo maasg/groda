9f59dd28d99aa1b12fcaf8d004ae952caebfbe17
Renato Cavalcanti <renato@strongtyped.io>
2016-11-23 21:24:59 +0100

    renamed EmptyRaffleTest

--- test/model/EmptyRaffleTest.scala
+++ test/model/EmptyRaffleTest.scala
@@ -7,17 +7,27 @@ import scala.util.Random
 /**
   * Created by Rutger Claes <rutger.claes@cegeka.be> on 23/11/16.
   */
-class EmptyRaffle$Test extends FlatSpec with Matchers {
+class EmptyRaffleTest extends FlatSpec with Matchers {
 
   def randomName = Random.nextString( 5 )
 
   def randomPrices = ( 1 to Random.nextInt( 10 ) + 1 ).map( i => Random.nextString( 5 ) ).toList
 
-  "EmptyRaffle" should "be open to new people" in {
+  behavior of "EmptyRaffle"
+
+  it should "be open to new people" in {
     val empty = EmptyRaffle( randomName, randomPrices )
     val notEmpty = empty.addPerson( randomName )
 
     notEmpty shouldBe a[DraftRaffle]
   }
+
+  it should "be open to new people" in {
+    val empty = EmptyRaffle( randomName, randomPrices )
+    val notEmpty = empty.addPerson( randomName )
+
+    notEmpty shouldBe a[DraftRaffle]
+  }
+
 
 }
