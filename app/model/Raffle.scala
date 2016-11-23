986ecb89a4be82f5e413eb4b418a65d63c3bbe26
Rutger Claes <rutger.claes@cegeka.be>
2016-11-23 21:17:16 +0100

    Refined the Raffle API.

--- app/model/Raffle.scala
+++ app/model/Raffle.scala
@@ -13,17 +13,42 @@ sealed trait Raffle {
 }
 
 object Raffle {
+
   type Outcome = (String,String)
+
+  def empty( name:String, prizes:List[String] ) = EmptyRaffle( name, prizes )
+
 }
 
-case class DraftRaffle( name:String, prizes:List[String] ) extends Raffle
+case class EmptyRaffle( name:String, prizes:List[String] ) extends Raffle {
 
-case class ReadyToRunRaffle( name:String, prizes:List[String], people:List[String] ) extends Raffle {
+  def addPerson( name:String ) = DraftRaffle( name, prizes, Set( name ) )
+
+}
+
+case class DraftRaffle( name:String, prizes:List[String], people:Set[String] ) extends Raffle {
+
+  def addPerson( name:String ) = {
+    val incPeople = people + name
+    if (incPeople.size >= prizes.size)
+      ReadyToRunRaffle(name, prizes, incPeople)
+    else
+      copy(people = incPeople)
+  }
+
+}
+
+case class ReadyToRunRaffle( name:String, prizes:List[String], people:Set[String] ) extends Raffle {
+
+  require( prizes.size >= people.size )
+
+  def addPerson( name:String ) = copy( people = people + name )
 
   def run:(Outcome,Raffle) = ???
 
   def finish:FinishedRaffle = ???
 
 }
+
 
 case class FinishedRaffle( name:String, outcome:List[Outcome] ) extends Raffle
